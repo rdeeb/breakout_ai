92fa9561d0797488e147b64276cc121f20181e26
Ramy Deeb <me@ramydeeb.com>
2017-05-25 15:39:49 -0500

    Basic Game

---
+++ src/js/game.js
@@ -0,0 +1,104 @@
+var game = new Phaser.Game(480, 320, Phaser.AUTO, null, {
+    preload: preload,
+    create: create,
+    update: update
+});
+
+var ball;
+var paddle;
+var bricks;
+
+function preload() {
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
+    game.stage.backgroundColor = '#EEE';
+
+    //Load the Assets
+    game.load.image('ball', 'public/images/ball.png');
+    game.load.image('paddle', 'public/images/paddle.png');
+    game.load.image('brick', 'public/images/brick.png');
+
+}
+
+function create() {
+    // World
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.arcade.checkCollision.down = false;
+
+    // Ball
+    ball = game.add.sprite(game.world.width*0.5, game.world.height-25, 'ball');
+    game.physics.enable(ball, Phaser.Physics.ARCADE);
+    ball.body.velocity.set(150, -150);
+    ball.body.collideWorldBounds = true;
+    ball.anchor.set(0.5);
+    ball.body.bounce.set(1);
+    ball.checkWorldBounds = true;
+    ball.events.onOutOfBounds.add(handleGameover, this);
+
+    // Paddle
+    paddle = game.add.sprite(game.world.width*0.5, game.world.height - 5, 'paddle');
+    paddle.anchor.set(0.5, 1);
+    game.physics.enable(paddle, Phaser.Physics.ARCADE);
+    paddle.body.immovable = true;
+
+    // Bricks
+    createBrickGrid();
+}
+
+function update() {
+    game.physics.arcade.collide(ball, paddle);
+    game.physics.arcade.collide(ball, bricks, whenBrickIsHit);
+    paddle.x = game.input.x || game.world.width*0.5;
+}
+
+function createBrickGrid() {
+    var brickInfo = {
+        width: 50,
+        height: 20,
+        count: {
+            row: 7,
+            col: 3
+        },
+        offset: {
+            top: 50,
+            left: 60
+        },
+        padding: 10
+    };
+    bricks = game.add.group();
+    for (var c=0; c<brickInfo.count.col; c++) {
+        for (var r=0; r<brickInfo.count.row; r++) {
+            var brickX = (r*(brickInfo.width+brickInfo.padding))+brickInfo.offset.left;
+            var brickY = (c*(brickInfo.height+brickInfo.padding))+brickInfo.offset.top;
+            var newBrick = game.add.sprite(brickX, brickY, 'brick');
+            game.physics.enable(newBrick);
+            newBrick.body.immovable = true;
+            newBrick.anchor.set(0.5);
+            bricks.add(newBrick);
+        }
+    }
+}
+
+function whenBrickIsHit(ball, brick) {
+    brick.kill();
+    var alive = 0;
+    for (var i=0; i < bricks.children.length; i++) {
+        if (bricks.children[i].alive === true) {
+            alive++;
+        }
+    }
+    if (alive == 0) {
+        handleFinished();
+    }
+}
+
+function handleGameover() {
+    alert('Game over!');
+    location.reload();
+}
+
+function handleFinished() {
+    alert('You did it!');
+    location.reload();
+}
